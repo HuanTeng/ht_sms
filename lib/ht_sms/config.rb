078de5b4e7ad329171dba47790199ec133ad68b7
delong <w.del@qq.com>
2014-12-15 15:29:03 +0800

    update config

--- lib/ht_sms/config.rb
+++ lib/ht_sms/config.rb
@@ -1,28 +1,23 @@
 module HtSms
 
-    Setting = defined?(Setting) ? Settings : {}
+  Setting = defined?(Setting) ? Settings : {}
 
-    class << self
+  class << self
 
-        def test_env(test=nil)
-            if test.nil?
-                @test ||= defined?(Rails) ? Rails.env.test? : true
-            else
-                @test = !!test
-            end
-        end
-
-        def config
-            if Emitter == DuanXinBao
-                dxb = defined?(Setting.duan_xin_bao) ? Setting.duan_xin_bao : nil
-                @dxb_config ||= {
-                    account: dxb && dxb.account,
-                    password: dxb && dxb.password,
-                    url: dxb && dxb.url
-                }
-            end
-        end
-
+    def test_env(test=nil)
+      if test.nil?
+        @test ||= defined?(Rails) ? Rails.env.test? : true
+      else
+        @test = !!test
+      end
     end
+
+    def config
+      if Emitter == DuanXinBao
+        @dxb_config ||= defined?(Setting.duan_xin_bao) ? Setting.duan_xin_bao : nil
+      end
+    end
+
+  end
 
 end
